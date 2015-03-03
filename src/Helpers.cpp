6d4352f9c683868c2f0b83ca273dc6afb7f118b7
thehollownike <nikolaus.heran@student.tugraz.at>
2015-03-03 20:46:07 +0100

    getting the time is now threadsafe

--- src/Helpers.cpp
+++ src/Helpers.cpp
@@ -16,8 +16,15 @@ void findAndReplaceAll(std::string& s,const std::string to_replace,const std::st
 std::string getTimeString()
 {
   time_t raw_time;
+  struct tm timeinfo;
+  char buffer[20];
   time(&raw_time);
-  std::string time_str = ctime(&raw_time);
+  localtime_s(&timeinfo, &raw_time);
+  strftime(buffer, 20, "%F_%H-%M-%S", &timeinfo);
+
+//  std::string time_str = ctime(&raw_time);
+  std::string time_str = std::string(buffer);
+  std::cout << "Time str " << time_str;
   time_str = time_str.erase(time_str.length()-1,1);
   findAndReplaceAll(time_str,std::string(" "),std::string("_"));
   findAndReplaceAll(time_str,std::string(":"),std::string("-"));
