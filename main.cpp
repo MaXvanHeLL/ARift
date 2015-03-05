c698a09d55a7946646ce72533c1f42bbd0ff42ff
MaXvanHeLL <mhoell@student.tugraz.at>
2015-03-05 14:50:37 +0100

    [minor Codechange] changed to window mode of the render scene

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ void render(ARiftControl* arift_c);
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 // not used currently
-const bool FULL_SCREEN = true;
+const bool FULL_SCREEN = false;
 const bool VSYNC_ENABLED = true;
 const float SCREEN_DEPTH = 1000.0f;
 const float SCREEN_NEAR = 0.1f;
